3c8c62fa1660b132b5300dcbfa70568d57f9677e
Passive14 <marcusjhomann@outlook.com>
2021-05-05 09:46:50 +0200

    asdasd

--- petclinic-selenium/.jenkins/modules/Deploy/Deploy.groovy
+++
@@ -1,9 +0,0 @@
-/**
- * Additional docker image deployment
- */
-
-MPLModule('Deploy', CFG)
-
-if( fileExists('Dockerfile') ) {
-  MPLModule('Docker Deploy', CFG)
-}
--- petclinic-selenium/.jenkins/modules/Test/Test.groovy
+++
@@ -1,9 +0,0 @@
-/**
- * Running selenium tests
- */
-
-if( ! CFG.'selenium.hub_url' ) {
-  MPLModule('Selenium Deploy', CFG)
-}
-
-MPLModule('Selenium Maven Test', CFG)
